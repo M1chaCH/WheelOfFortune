3026b15c298f44162a2a9a3acb6a92d556f574c7
Micha Schweizer <micha_ch@outlook.com>
2022-03-15 13:25:50 +0100

    initialize spring

---
+++ backend/src/test/java/ch/bbbaden/m151/wheeloffortune/WheelOfFortuneApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbbaden.m151.wheeloffortune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelOfFortuneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
