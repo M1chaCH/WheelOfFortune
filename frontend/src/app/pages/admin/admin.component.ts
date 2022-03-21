d985d8ec7ae38c0bcf379d4760532e55ce65bbcb
Micha Schweizer <micha_ch@outlook.com>
2022-03-21 15:35:51 +0100

    small ui improvements - show username when logged in

--- frontend/src/app/pages/admin/admin.component.ts
+++ frontend/src/app/pages/admin/admin.component.ts
@@ -1,19 +1,30 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 import {AppRout} from "../../config/appRout";
 import {AuthService} from "../../auth/auth.service";
+import {WheelOfFortuneApiService} from "../../api/wheel-of-fortune-api.service";
+import {ApiEndpoint} from "../../config/apiEndpoint";
+import {ApiHttpMethods} from "../../config/apiHttpMethods";
 
 @Component({
   selector: "admin",
   templateUrl: "admin.component.html",
   styleUrls: ["admin.component.scss"]
 })
-export class AdminComponent{
+export class AdminComponent implements OnInit{
+
+  username: string = "";
 
   constructor(
     private router: Router,
     private auth: AuthService,
+    private api: WheelOfFortuneApiService,
   ) { }
+
+  ngOnInit(): void {
+    this.api.callHandled(ApiEndpoint.AUTH, {}, ApiHttpMethods.GET, true)
+      .subscribe((name: any) => this.username = name.response);
+  }
 
   goHome(){
     this.router.navigate([AppRout.HOME]);
