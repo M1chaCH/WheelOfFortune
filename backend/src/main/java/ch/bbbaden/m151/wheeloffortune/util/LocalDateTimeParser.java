35f8df8221daa4c42af4200c457f189fad56f905
Micha Schweizer <micha_ch@outlook.com>
2022-03-21 15:18:20 +0100

    add date to HighScore - fix date to be parsable in TS

--- backend/src/main/java/ch/bbbaden/m151/wheeloffortune/util/LocalDateTimeParser.java
+++ backend/src/main/java/ch/bbbaden/m151/wheeloffortune/util/LocalDateTimeParser.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeParser {
-    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private LocalDateTimeParser() { }
 
